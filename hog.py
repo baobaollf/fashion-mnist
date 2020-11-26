5ff86332096480cd32ade9d189c2a63e15b4f99e
baobaollf <55776365+baobaollf@users.noreply.github.com>
2020-11-25 20:37:23 -0600

    knn from sklearn

--- hog.py
+++ hog.py
@@ -11,6 +11,15 @@ import matplotlib.pyplot as plt
 import os
 import math
 from skimage.feature import hog
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+
+def knn_from_library(k, x_train, y_train):
+    x_train = hog_for_array(x_train)
+    model = KNeighborsClassifier(n_neighbors=k)
+    model.fit(x_train, y_train)
+    return model
 
 
 def load_mnist(path, kind='train'):
@@ -43,18 +52,29 @@ def hog_for_array(image_array):
                  pixels_per_cell=(7, 7),
                  cells_per_block=(4, 4))
         hog_features_data.append(fd)
-        plt.hist(fd)
-        plt.show()
+        # plt.hist(fd)
+        # plt.show()
     hog_features = np.array(hog_features_data, 'float64')
     return np.float32(hog_features)
+
+
+def predict(model, x_test):
+    x_test = hog_for_array(x_test)
+    return model.predict(x_test)
 
 
 def main():
     plt.gray()
     x_train, y_train = load_mnist('fashion-mnist/data/fashion', kind='train')
     x_test, y_test = load_mnist('fashion-mnist/data/fashion', kind='t10k')
-    hog_for_array(x_train[0:10])
+    # feature = hog_for_array(x_train[0:10])
+    # label = y_train[0:10]
 
+    model = knn_from_library(3, x_train, y_train)
+    y_pred = predict(model, x_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    # print("prediction is", model.predict(hog_for_array(x_test[0:1])))
+    # print("true label is", y_test[0])
 
 
 if __name__ == '__main__':
